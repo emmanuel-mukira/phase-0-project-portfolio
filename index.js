2d14b19f665c8773358a66b7babdc05d7bc729e7
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-03-10 10:26:13 +0300

    Particles Atleast

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      speed: 3,
+      resumeAnimation: true,
+      maxParticle: 300,
+    });
+  };
